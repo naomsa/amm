4cbeec541a23ba095ddef230d1020edf00e27f94
naomsa <djbrucelee@protonmail.com>
2022-06-30 18:52:39 -0300

    feat: added a deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const greeter = await (
-    await ethers.getContractFactory("Greeter")
-  ).deploy("Hello, World!");
+  const token0 = await (await ethers.getContractFactory("MockERC20")).deploy();
+  const token1 = await (await ethers.getContractFactory("MockERC20")).deploy();
 
-  await greeter.deployed();
-  console.log("Greeter: ", greeter.address);
+  const amm = await (
+    await ethers.getContractFactory("AMM")
+  ).deploy(token0.address, token1.address);
+
+  await amm.deployed();
+
+  console.log("token0:", token0.address);
+  console.log("token1:", token1.address);
+  console.log("amm:", amm.address);
 }
 
 main().catch((error) => {
